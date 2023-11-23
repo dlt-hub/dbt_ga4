66330b0002f2dfdadb8ce8fa9a7b9219f6c1d4fb
AstrakhantsevaAA <astra92293@gmail.com>
2023-11-23 13:54:57 +0100

    make it cross-compatible

--- dbt_transform/models/staging/stg_event_params_unpacked.sql
+++ dbt_transform/models/staging/stg_event_params_unpacked.sql
@@ -4,8 +4,8 @@ WITH CTE AS (
     e.event_date,
     e.event_timestamp,
     e.event_name,
-    ep.key,
-    COALESCE(CAST(ep.value__int_value AS STRING), ep.value__string_value) as value
+    ep.key as key_value,
+    COALESCE(CAST(ep.value__int_value AS {{ dbt.type_string() }}), ep.value__string_value) as value
   FROM  {{ source("ga4_select_star", "events") }} e
   JOIN  {{ source("ga4_select_star", "events__event_params") }} ep
   ON e._dlt_id = ep._dlt_parent_id
@@ -15,15 +15,14 @@ SELECT
     event_id,
     event_date,
     event_timestamp,
-    event_name,
+    event_name
     {% for param in var('event_params') %}
-        MAX(
-            CASE
-                WHEN key = '{{ param }}' THEN value
-            END
-        ) as {{ param }},
+    , MAX(
+        CASE
+            WHEN key_value = '{{ param }}' THEN value
+        END
+    ) as {{ param }}
     {% endfor %}
-
 FROM CTE
 GROUP BY event_id, event_date, event_timestamp, event_name
 ORDER BY event_date, event_timestamp
--- dbt_transform/models/staging/stg_events.sql
+++ dbt_transform/models/staging/stg_events.sql
@@ -1,7 +1,7 @@
 SELECT
-    _dlt_id as event_id,
+    _dlt_id as event_id
     {% for col in get_filtered_columns(from=source("ga4_select_star", "events"), except=["_dlt_id"])  %}
-    {{ col }} as {{ col }},
+    , {{ col }} as {{ col }}
     {% endfor %}
 FROM
     {{ source("ga4_select_star", "events") }}
